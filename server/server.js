a980a91d777e84631f10317989ce5025076b0d21
Rajankumar001 <chaudharyrajan387@gmail.com>
2024-11-24 15:49:25 +0530

    changes tracked in server.js page in line 15

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT=3000;
 app.use(express.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api/User', googleAuthRoutes);
+app.use('/', googleAuthRoutes);
 app.use('/api/User',UserRouter);
 
 
